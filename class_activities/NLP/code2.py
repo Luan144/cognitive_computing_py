693f3586f68693c26ab4e9b8ab07673bd2a535ff
Luan Gomes <luan.gomes@dellead.com>
2022-04-30 18:57:11 -0300

    - Add NLP module directories - Add 30/04/2022 class activities

---
+++ class_activities/NLP/code2.py
@@ -0,0 +1,19 @@
+# Import necessary modules
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from NLP.src.nlp_utils import get_sample_Santo_Graal
+
+nltk.download('punkt')
+
+# Split scene_one into sentences: sentences
+scene_one = get_sample_Santo_Graal()
+sentences = sent_tokenize(scene_one)
+
+# # Use word_tokenize to tokenize the fourth sentence: tokenized_sent
+tokenized_sent = word_tokenize(sentences[3])
+
+# Make a set of unique tokens in the entire scene: unique_tokens
+unique_tokens = set(tokenized_sent)
+
+# Print the unique tokens result
+print(unique_tokens)
